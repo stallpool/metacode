c49d1195c316722e9823a7712852f47e4352609b
Seven Lju <dna2gle@gmail.com>
2021-03-12 21:54:40 +0800

    add p4 search path

--- src/p4.js
+++ src/p4.js
@@ -24,6 +24,8 @@ const api = {
       options = options || {};
       if (!options.query || !options.path || !options.server) throw 'bad request';
       if (options.changenumber) options.changenumber = parseInt(options.changenumber, 10);
+      options.ignoreCase = !!options.ingnoreCase;
+      const ignoreCase = options.ignoreCase?'-i ':'';
       const prefix = P4USER?`P4USER=${P4USER} `:'';
       const suffix = options.changenumber?`@${options.changenumber}`:'';
       // shell safe transform: 'test' -> '"'"'test'"'"'
@@ -31,8 +33,8 @@ const api = {
       // TODO: normalize query, path, server to make sure safe
       // TODO: how to deal with long line, for example json in one line, where length > 1G
       const cmd = (
-         `${prefix}P4PORT=${options.server} ${P4BIN} grep -n -A 1 -B 1 -e '${query}' ` +
-         `${options.path}${suffix}`
+         `${prefix}P4PORT=${options.server} ${P4BIN} grep -s -n -A 1 -B 1 -F ${ignoreCase} ` +
+         `-e '${query}' ${options.path}${suffix}`
       );
       const env = { err: false };
       const items = [{}]; // { ref, path, data, startline }
@@ -83,6 +85,70 @@ const api = {
          items
       };
    }, // search
+   searchPath: async (options) => {
+      // p4 files '//.../*test*'
+      /*
+       * The -e flag displays files with an action of anything other than
+       * deleted, purged or archived.  Typically this revision is always
+       * available to sync or integrate from.
+       *
+       * The -i flag is used to ignore the case of the file argument when
+       * listing files in a case sensitive server.
+       *
+       * The -m flag limits files to the first 'max' number of files.
+       */
+      options = options || {};
+      if (!options.query || !options.path || !options.server) throw 'bad request';
+      if (options.changenumber) options.changenumber = parseInt(options.changenumber, 10);
+      options.n = parseInt(options.n || '50', 10);
+      if (options.n > 200) options.n = 200;
+      options.ignoreCase = !!options.ingnoreCase;
+      const ignoreCase = options.ignoreCase?'-i ':'';
+      const prefix = P4USER?`P4USER=${P4USER} `:'';
+      const suffix = options.changenumber?`@${options.changenumber}`:'';
+      // shell safe transform: 'test' -> '"'"'test'"'"'
+      const query = `${options.path}/.../*${options.query.replace(/'/g, '\'"\'"\'').replace(/[\n]/g, '\\n')}*${suffix}`;
+      // TODO: normalize query, path, server to make sure safe
+      // TODO: do we need to exclude stat=[delete]
+      const cmd = `${prefix}P4PORT=${options.server} ${P4BIN} files ${ignoreCase} -m ${options.n} '${query}'`;
+      const env = { err: false };
+      const items = []; // { ref, path, data, startline }
+      await i_exec.act(cmd, (line) => {
+         if (!line) return;
+         const env = { parts: null };
+         env.parts = line.split('#');
+         const path = env.parts[0];
+         env.parts = env.parts[1].split(/[ \-]+/);
+         const rev = env.parts[0];
+         const stat = env.parts[1];
+         const ref = env.parts[3];
+         const item = {
+            path,
+            ref: parseInt(ref, 10),
+            rev: parseInt(rev, 10),
+            startline: -1, data: null,
+         };
+         items.push(item);
+
+      }).catch((err) => {
+         if (err.message.indexOf('Perforce password (P4PASSWD) invalid or unset.') >= 0) {
+            env.err = 'bad config';
+            console.error('[!] p4 tickets are expired ...');
+            return;
+         }
+         env.err = err;
+      });
+      if (env.err) throw env.err;
+      return {
+         meta: {
+            n: items.length,
+            page: 1,
+            total: items.length,
+            total_page: 1
+         },
+         items
+      };
+   }, // searchPath
 };
 
 module.exports = api;
